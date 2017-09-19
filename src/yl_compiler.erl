6ddeabf82cb79eedb459b0c7ca6fb6892384d72e
Paweł Obrok <pawel.obrok@gmail.com>
2017-09-19 18:31:32 +0200

    Remove debug prints

--- src/yl_compiler.erl
+++ src/yl_compiler.erl
@@ -5,10 +5,7 @@
 compile(InputBinary) ->
   ListContent = binary:bin_to_list(InputBinary),
   {ok, Tokens, _LastLine} = yl_tokenizer:string(ListContent),
-  io:format("~p~n", [Tokens]),
   {ok, Parsed} = yl_parser:parse(Tokens),
-  io:format("~p~n", [Parsed]),
   {ok, Mod, Binary} = yl_codegen:generate(Parsed),
-  io:format("~p~n", [Mod]),
   code:load_binary(Mod, 'test.yl', Binary),
   {ok, Mod}.
